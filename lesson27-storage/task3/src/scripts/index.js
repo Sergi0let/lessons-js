dd5906f5f71d4da816c353f1acbc17cbec163e7b
Sergi0let <s.vashkevych@gmail.com>
2022-07-12 20:16:33 +0300

    init

--- lesson27-storage/task3/src/scripts/index.js
+++ lesson27-storage/task3/src/scripts/index.js
@@ -1,13 +1,10 @@
 import { initTodoListHandlers } from './todoList.js';
 import { renderTasks } from './renderer.js';
-import { getItem, setItem } from './storage.js';
 
-// put your code here
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   renderTasks();
-//   initTodoListHandlers();
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  renderTasks();
+  initTodoListHandlers();
+});
 
 // const onStarageChange = e => {
 //   if (e.key === 'tasksList') {
