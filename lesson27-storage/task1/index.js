1f2e305b631df7116739aa72833b4325eb409837
Sergi0let <s.vashkevych@gmail.com>
2022-07-12 16:54:44 +0300

    init

---
+++ lesson27-storage/task1/index.js
@@ -0,0 +1,37 @@
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+localStorage.setItem('name', 'Tom');
+localStorage.setItem('age', JSON.stringify(17));
+
+console.log('LocalStorageData: ', localStorage);
+
+const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    return {
+      ...acc,
+      [key]: value,
+    };
+  }, {});
+};
+console.log('function getLocalStorageData: ', getLocalStorageData());
+
+console.log();
+
+const user = {
+  name: 'Tom',
+  age: 17,
+  isStudent: false,
+  driverLicense: null,
+  hobbies: ['football', 'diving'],
+  education: [
+    {
+      name: 'MIT precourse',
+      graduateDate: '2020-05-04T14:48:46.105Z',
+    },
+  ],
+};
