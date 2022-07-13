c337ed5db330d5fd1fb1544ae71d0e6f75a75158
Sergi0let <s.vashkevych@gmail.com>
2022-07-13 22:15:00 +0300

    init

--- lesson29-async/task1/index.js
+++ lesson29-async/task1/index.js
@@ -1,2 +1,48 @@
+// addImage
+/*
 const buttonElem = document.querySelector('#button');
-// buttonElem.addEventListener('click', () => cons);
+
+buttonElem.addEventListener('click', () => console.log('Click me'));
+
+let sum = 0;
+const startTime = new Date();
+
+for (let i = 0; i < 1e9; i++) {
+  sum += i;
+}
+
+const endTime = new Date();
+console.log(`Cycle took ${endTime - startTime}ms`);
+*/
+
+const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  const containerElem = document.querySelector('.page');
+
+  imgElem.setAttribute('alt', 'My photo');
+  imgElem.src = imgSrc;
+
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener('load', onImageLoaded);
+  imgElem.addEventListener('error', () => 'Image load fail');
+};
+
+const imgSrc =
+  'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson24/hw1/illustration-window.pn';
+
+const onImageLoaded = (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = data;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+addImage(imgSrc, onImageLoaded);
