b5674dae6bc60029024cfbde926ccea20460c354
Sergi0let <s.vashkevych@gmail.com>
2022-07-13 12:11:25 +0300

    init

--- lesson27-storage/task4/index.js
+++ lesson27-storage/task4/index.js
@@ -1,60 +1,57 @@
-let tasks;
-!localStorage.tasks ? (tasks = []) : (tasks = JSON.parse(localStorage.getItem('tasks')));
+const addTaskBtn = document.querySelector('.create-task-btn');
+const deskTaskInput = document.querySelector('.task-input');
+const toDoList = document.querySelector('.list');
 
-const updateLocaleStorage = () => {
+let tasks = [];
+!localStorage.tasks ? [] : (tasks = JSON.parse(localStorage.getItem('tasks')));
+
+let todoItemElems = [];
+console.log(todoItemElems);
+
+function Task(description) {
+  this.description = description;
+  this.done = false;
+}
+
+const createTemplate = (task, index) => {
+  return `
+     <li class="list__item" ${task.done ? 'checked' : ''}>
+       <input onclick="doneTask(${index})" class="list__item-checkbox" type="checkbox" 
+       ${task.done ? 'checked' : ''}/>
+       <div class="description">${task.description}j</div>
+     </li>
+  `;
+};
+
+const fillHtmlList = () => {
+  toDoList.innerHTML = '';
+  if (tasks.length > 0) {
+    tasks.forEach((item, index) => {
+      toDoList.innerHTML += createTemplate(item, index);
+    });
+    todoItemElems = document.querySelectorAll('.list__item');
+  }
+};
+fillHtmlList();
+
+const updateLocal = () => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
-const listElem = document.querySelector('.list');
-
-const renderTasks = tasksList => {
-  console.log(tasksList);
-  const tasksElems = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done }, index) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.setAttribute('data-num', index);
-
-      checkbox.addEventListener('click', function (event) {
-        const currentBox = +event.target.dataset.num;
-        if (tasks[currentBox].done) {
-          tasks[currentBox].done = false;
-        } else {
-          tasks[currentBox].done = true;
-        }
-        listElem.innerHTML = '';
-
-        renderTasks(tasks);
-      });
-      checkbox.checked = done;
-      checkbox.classList.add('list__item-checkbox');
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      listItemElem.append(checkbox, text);
-      return listItemElem;
-    });
-
-  listElem.append(...tasksElems);
-};
-renderTasks(tasks);
-
-const createTask = document.querySelector('.create-task-btn');
-const taskInput = document.querySelector('.task-input');
-function getNewTask() {
-  if (!taskInput.value) {
-    return;
+const doneTask = index => {
+  tasks[index].done = !tasks[index].done;
+  if (tasks[index].done) {
+    todoItemElems[index].classList.add('list__item_done');
+  } else {
+    todoItemElems[index].classList.remove('list__item_done');
   }
+  updateLocal();
+  // fillHtmlList();
+};
 
-  const createToDo = { text: taskInput.value, done: false };
-  tasks.push(createToDo);
-  updateLocaleStorage();
-  console.log(tasks);
-  taskInput.value = '';
-  listElem.innerHTML = '';
-  renderTasks(tasks);
-}
-createTask.addEventListener('click', getNewTask);
+addTaskBtn.addEventListener('click', () => {
+  tasks.push(new Task(deskTaskInput.value));
+  updateLocal();
+  fillHtmlList();
+  deskTaskInput.value = '';
+});
