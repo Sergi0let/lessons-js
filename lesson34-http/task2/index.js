6b3a516e3bfc8f42f6f572f472c96b58d523a45a
Sergi0let <s.vashkevych@gmail.com>
2022-07-20 15:00:22 +0300

    init

--- lesson34-http/task2/index.js
+++ lesson34-http/task2/index.js
@@ -1,7 +1,39 @@
-// page
-// login-form
-// form-title
-// form-control
-// form-label
-// form-input
-// submit-button
+const baseUrl = 'https://62d7ad6c49c87ff2af3a6350.mockapi.io/api/v1/users';
+
+const formElem = document.querySelector('.login-form');
+const buttonElem = document.querySelector('.submit-button');
+
+const createUser = userData => {
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(response => {
+      return 'good';
+    })
+    .then(data => alert({ data }));
+};
+
+const checkInputs = () => {
+  const emailInElem = document.querySelector('input[type="email"]').reportValidity();
+  const userInElem = document.querySelector('input[type="text"]').reportValidity();
+  const passwordInElem = document.querySelector('input[type="password"]').reportValidity();
+
+  if (userInElem && passwordInElem && emailInElem) {
+    buttonElem.removeAttribute('disabled');
+  }
+};
+
+const readUserData = () => {
+  createUser(Object.fromEntries(new FormData(formElem)));
+  formElem.reset();
+};
+
+formElem.addEventListener('input', checkInputs);
+buttonElem.addEventListener('click', readUserData);
+buttonElem.addEventListener('click', () => buttonElem.setAttribute('disabled', false));
+
+// buttonElem.addEventListener('click', e => e.preventDefault());
