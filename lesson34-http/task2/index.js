45a7d19034da39c662b4b9e22babaa174cf8ea07
Sergi0let <s.vashkevych@gmail.com>
2022-07-20 11:17:13 +0300

    init

---
+++ lesson34-http/task2/index.js
@@ -0,0 +1,7 @@
+// page
+// login-form
+// form-title
+// form-control
+// form-label
+// form-input
+// submit-button
---
+++ lesson34-http/task3/index.js
@@ -0,0 +1,72 @@
+const baseUrl = 'https://62d7ad6c49c87ff2af3a6350.mockapi.io/api/v1/users';
+
+function getUsersList() {
+  return fetch(baseUrl).then(response => response.json());
+}
+
+export function getUserById(userId) {
+  return fetch(`${baseUrl}/${userId}`).then(response => response.json());
+}
+
+export function createUser(userData) {
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+export function deleteUser(userId) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'DELETE',
+  });
+}
+
+export function updateUser(userId, userData) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+// examples
+// getUsersList().then(users => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
+
+// getUserById('2').then(userData => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+// });
+
+// const newUserData = {
+//   email: 'cool@email.com',
+//   firstName: 'Iron',
+//   lastName: 'Man',
+//   age: 42,
+// };
+
+// createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
+
+// const updatedUserData = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
+
+// updateUser('3', updatedUserData).then(() => {
+//   console.log('User updated');
+// });
+
+// deleteUser('55').then(() => {
+//   console.log('User updated');
+// });
+
+// -----------
