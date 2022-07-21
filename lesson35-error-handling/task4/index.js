1138bb05e68103a7fea01ceb0d158475b11aa722
Sergi0let <s.vashkevych@gmail.com>
2022-07-21 14:25:45 +0300

    init

--- lesson35-error-handling/task4/index.js
+++ lesson35-error-handling/task4/index.js
@@ -1,26 +1,38 @@
-const userAvatarElem = document.querySelector('.user__avatar');
-const userNameElem = document.querySelector('.user_name');
-const userLocationElem = document.querySelector('.user__location');
+import { fetchRepositories, fetchUserData } from './gateWays.js';
+import { renderUserData } from './user.js';
+import { renderRepos, cleanReposList } from './repos.js';
+import { showSpinner, hideSpinner } from './spinner.js';
 
-const defaultElem = 'https://avatars3.githubusercontent.com/u10001';
-
-userAvatarElem.src = defaultElem;
-
-const fetchUserData = userName => {
-  return fetch(`https://api.github.com/users/${userName}`).then(response => response.json());
+const defaultUser = {
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  name: '',
+  location: '',
 };
 
-const renderUserData = userData => {
-  const { avatar_url, name, location } = userData;
-  userNameElem.textContent = name;
-  userAvatarElem.src = avatar_url;
-  userLocationElem.textContent = location ? `from ${location}` : '';
-};
+renderUserData(defaultUser);
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
 
 const onSearchUser = () => {
+  showSpinner();
+  // cleanReposList();
   const userName = userNameInputElem.value;
-  fetchUserData(userName).then(userData => renderUserData(userData));
+  fetchUserData(userName)
+    .then(userData => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then(url => fetchRepositories(url))
+    .then(reposList => {
+      renderRepos(reposList);
+    })
+    .catch(err => {
+      alert(err.message);
+    })
+    .finally(() => {
+      hideSpinner();
+    });
 };
+
+showUserBtnElem.addEventListener('click', onSearchUser);
