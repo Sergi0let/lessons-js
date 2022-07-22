f970649dc20693d2f31a1f93e2c0f8d39f0cba2c
Sergi0let <s.vashkevych@gmail.com>
2022-07-22 11:22:31 +0300

    init

---
+++ lesson36-async-await/task1/index.js
@@ -0,0 +1,33 @@
+/*
+async function func() {
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      resolve(17);
+    }, 1000);
+  });
+
+  const result = await promise;
+  console.log(result);
+  return 1;
+}
+
+const result = func();
+console.log('Hi');
+*/
+
+const getUser = async userId => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (!response.ok) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to get user data');
+  }
+};
+
+getUser('facebook')
+  .then(userData => console.log(userData))
+  .catch(err => alert(err.message));
